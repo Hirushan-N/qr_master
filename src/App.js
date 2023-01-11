dfec5fb7d23390fc353edf4802240869c2bf303d
Hirushan-N <nadeeshhirushan@gmail.com>
2023-01-11 14:15:05 +0530

    QR generator completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import QRCode from "react-qr-code";
 
 function App() {
+  const [inputValue,setInputValue] = useState("")
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      npm add qr-scanner react-qr-code
+      <h3>QR Generator and Downloader</h3>
+
+      <input type="text" onChange={(e)=>setInputValue(
+        e.target.value
+      )} ></input>
+
+      <div style={{ height: "auto", margin: "20 auto",padding:"50px", maxWidth: 200, width: "100%" }}>
+        <QRCode
+          size={256}
+          style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+          value={inputValue}
+          viewBox={`0 0 256 256`}
+        />
+      </div>
+
+
+
+      <h3>QR Reader</h3>
     </div>
   );
 }
